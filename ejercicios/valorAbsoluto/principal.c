e1b195e6b83df70641339166c0861895e8ff4951
Chalo Salvador <chalosalvador@gmail.com>
2016-08-02 07:28:10 -0500

    clase 16

---
+++ ejercicios/valorAbsoluto/principal.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include "funciones.h"
+// programa principal
+
+int main() {
+    double v, a;
+    printf("Ingrese un valor numerico: ");
+    scanf("%lf", &v);
+    // invoco a la funcion
+    a = valorAbsoluto(v);
+    printf("El valor absoluto de %lf es %lf\n", v, a);
+    return 0;
+}
